b454758789bf0c4ec488fb3dec15569d08f7eb82
AhmetAkcil <ahmetakcil@outlook.com.tr>
2022-11-25 15:02:49 +0300

    GridBuilding

---
+++ Assets/Data/BuildingSO.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BuildData",menuName = "Build")]
+public class BuildingSO : ScriptableObject
+{
+    public string name;
+    public Transform prefab;
+    public Transform visual;
+    public int width;
+    public int height;
+
+    public enum Dir
+    {
+        Down,
+        Left,
+        Up,
+        Right
+    }
+
+    public List<Vector2Int> GetGridPositionList(Vector2Int offSet, Dir dir)
+    {
+        List<Vector2Int> gridPositionList = new List<Vector2Int>();
+        switch (dir)
+        {
+            default:
+                case Dir.Down:
+                case Dir.Up:
+                    for (int x = 0; x < width; x++)
+                    {
+                        for (int y = 0; y < height; y++)
+                        {
+                            gridPositionList.Add(offSet+new Vector2Int(x,y));
+                        }
+                    }
+                    break;
+                case Dir.Left:
+                case Dir.Right:
+                    for (int x = 0; x < height; x++)
+                    {
+                        for (int y = 0; y < width; y++)
+                        {
+                            gridPositionList.Add(offSet+new Vector2Int(x,y));
+                        }
+                    }
+                    break;
+        }
+
+        return gridPositionList;
+    }
+}
---
+++ Assets/Scripts/GridBuild&Creator/GridBuilding.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using Utils;
+public class GridBuilding : MonoBehaviour
+{
+    public BuildingSO buildingSo;
+    private GridCreator GridCreator;
+
+    private void Start()
+    {
+        GridCreator = GetComponent<GridCreator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            Vector3 mousePosition = UtilsMethod.GetMouseWorldPosition();
+            GridCreator.pathfinding.GetGrid().GetXY(mousePosition, out int x, out int z);
+            Vector3 placedObjectWorldPosition =   GridCreator.pathfinding.GetGrid().GetWorldPosition(x, z);//build objenin inşa edilecek tıklama konumu
+
+            List<Vector2Int> buildObjectgridPosList= buildingSo.GetGridPositionList(new Vector2Int(x, z), BuildingSO.Dir.Down);//build objenin kaplayacağı alanlar listesi
+
+            bool canBuild = true;
+            foreach (var VARIABLE in buildObjectgridPosList)
+            {
+                if ( !GridCreator.pathfinding.GetNode(VARIABLE.x, VARIABLE.y).CanBuild())
+                {
+                    //cannot build here
+                    canBuild = false;
+                    break;
+                }
+            }
+            
+            if (canBuild)
+            {
+                var build=Instantiate(buildingSo.prefab,placedObjectWorldPosition,quaternion.identity);
+                
+                foreach (var VARIABLE in buildObjectgridPosList)
+                {
+                    GridCreator.pathfinding.GetNode(VARIABLE.x, VARIABLE.y).SetBuilding(build.transform);
+                }
+                //GridCreator.pathfinding.GetNode(x, z).SetBuilding(build.transform);
+            }
+            else
+            {
+                Debug.Log("build varr");
+            }
+           
+           
+        }
+    }
+
+    
+ 
+}
