a7bf9c1ae8de4511a62dd3a31174e01682466504
AhmetAkcil <ahmetakcil@outlook.com.tr>
2022-12-04 13:57:05 +0300

    soldier sprites & anim added

--- Assets/Scripts/AStar/CharacterPathfindingMovementHandler.cs
+++ Assets/Scripts/AStar/CharacterPathfindingMovementHandler.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,8 @@ public class CharacterPathfindingMovementHandler : MonoBehaviour {
     private const float speed = 40f;
     private int currentPathIndex;
     private List<Vector3> pathVectorList;
+
+    [Header("animator")] [SerializeField] private Animator animator;
 
     
     private void Update() {
@@ -35,6 +38,8 @@ public class CharacterPathfindingMovementHandler : MonoBehaviour {
                 currentPathIndex++;
                 if (currentPathIndex >= pathVectorList.Count) {
                     StopMoving();
+                    AnimatorState("Run", false);
+
                     CharacterPathfindingMove.Instance.ClearCharacterPathfinding();
 
                 }
@@ -54,7 +59,26 @@ public class CharacterPathfindingMovementHandler : MonoBehaviour {
         return transform.position;
     }
 
-    public void SetTargetPosition(Vector3 targetPosition) {
+    public void SetTargetPosition(Vector3 targetPosition)
+    {
+        Debug.Log("target pos "+targetPosition);
+        Debug.Log("target pos not "+transform.position);
+        float dir = targetPosition.x - transform.position.x;
+        Debug.Log("target pos dir "+dir);
+        Vector3 soldierDir = animator.gameObject.transform.localScale;
+
+        if (dir < 0 && soldierDir.x>0)
+        {
+            soldierDir.x *= -1;
+            animator.gameObject.transform.localScale = soldierDir;
+        }
+        if (dir > 0 && soldierDir.x<0)
+        {
+            soldierDir.x *= -1;
+            animator.gameObject.transform.localScale = soldierDir;
+        }
+       
+        AnimatorState("Run", true);
         currentPathIndex = 0;
         pathVectorList = Pathfinding.Instance.FindPath(GetPosition(), targetPosition);
 
@@ -63,4 +87,9 @@ public class CharacterPathfindingMovementHandler : MonoBehaviour {
         }
     }
 
+
+    void AnimatorState(String tempString, bool state)
+    {
+        animator.SetBool(tempString,state);
+    }
 }
